f6f8ec5a0363412e0c12c0148cf4add09667ca30
Valentino Ekaputra <silentrazgriz@gmail.com>
2016-04-14 19:02:14 +0700

    init

---
+++ Assets/Script/AnimationTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTest : MonoBehaviour {
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	   anim = GetComponent <Animator> ();
+	}    
+    public void Jump () {
+        anim.SetTrigger ("isJump");
+    }
+}
